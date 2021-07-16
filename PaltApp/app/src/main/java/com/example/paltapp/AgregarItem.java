dca94d3141b60e0a1ce3d473528684a24cb6b035
cavigna <40524472+cavigna@users.noreply.github.com>
2021-07-15 22:24:34 -0400

    Base PaltApp

---
+++ PaltApp/app/src/main/java/com/example/paltapp/AgregarItem.java
@@ -0,0 +1,14 @@
+package com.example.paltapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_item);
+    }
+}
